e1c6c024f1ba333b074699b0bf9e099597a7deae
valentinepetrov <w.e.r.9.9.9@mail.ru>
2019-09-24 09:22:28 +0300

    add : solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let phrase = "";
+  let code = "";   
+  
+  for (let s = 0; s <= expr.length; s += 10) { 
+    let value = expr.substr(s, 10);
+           
+    for (let i = 0; i < value.length; i++) {
+      if (value[i] == 1 & value[i + 1] == 1) {
+      code += "-";
+      i++;   
+      }
+      if (value[i] == 1 & value[i + 1] == 0) {
+      code += ".";
+      i++;
+      }
+      if (value[i] == 1 & value[i + 1] == undefined & (i + 1) !== value.length) {
+      code += ".";
+      i++;    
+      }  
+    }
+  
+  phrase += MORSE_TABLE[ code ] ? MORSE_TABLE[ code ] : " ";
+  code = "";  
+  }
+  return phrase = phrase.trim();
 }
 
 module.exports = {
